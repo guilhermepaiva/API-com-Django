8713ab4c8e57901b4942131d57b23c839f4ebb3f
guilhermepaiva <guilhermpaiva9@gmail.com>
2020-08-04 09:17:15 -0300

    modelo de aluno e de curso, config do admin e serializacao dos dados

---
+++ escola/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from escola.models import Aluno, Curso
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+
